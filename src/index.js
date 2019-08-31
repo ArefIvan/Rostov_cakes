1547c76ab3e9537f9230fc121829eab033a0bdbf
Aris <aref.ivan@gmail.com>
2019-08-31 22:39:43 +0300

    slide

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './scss/main.scss';
+import $ from "jquery";
+import "owl.carousel";
+import 'owl.carousel/dist/assets/owl.carousel.css';
+$(".slider--main").owlCarousel({
+    items:1,
+    loop:true,
+    nav:true,
+})
